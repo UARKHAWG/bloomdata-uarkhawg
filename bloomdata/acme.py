f9ef824735461a1b33800902dca86b24956b093e
Kevin Bridges <110652476+UARKHAWG@users.noreply.github.com>
2023-05-04 17:33:36 -0500

    Add files via upload

--- bloomdata/acme.py
+++ bloomdata/acme.py
@@ -1,56 +1,53 @@
 '''Imports'''
-#import pytest
 import random
 
-#class Product:
 
 class Product:
     '''Create Product class'''
-    price = 10
-    weight = 20
-    flammability = 0.5
-    def __init__(self, name, identifier, price, weight, flammability):
+    def __init__(self,
+                 name,
+                 identifier=random.randint(1000000, 9999999),
+                 price=10,
+                 weight=20,
+                 flammability=0.5) -> None:
         self.name = name
+        self.identifier = identifier
         self.price = price
         self.weight = weight
         self.flammability = flammability
-        self.identifier = random.randint(1000000, 9999999)
-
-
 
     def stealability(self):
         '''func method docstring'''
         if (self.price / self.weight) < 0.5:
             return 'Not so stealable'
-        elif ((self.price / self.weight) >= 0.5) & ((self.price / self.weight) < 1):
+        if ((self.price / self.weight) >= 0.5) \
+                & ((self.price / self.weight) < 1):
             return 'Kinda stealable'
-        else:
-            return 'Very stealable!'
+        return 'Very stealable!'
 
     def explode(self):
         '''func method docstring'''
         if (self.flammability * self.weight) < 10:
             return 'fizzle'
-        if ((self.flammability * self.weight) >= 10) & ((self.flammability * self.weight) < 50):
+        if ((self.flammability * self.weight) >= 10) \
+                & ((self.flammability * self.weight) < 50):
             return 'boom!'
         return 'BABOOM!!'
 
-if __name__ == '__main__':
-    prod = Product('soap', 0, 100, 27.2, 2.3)
-
-    print(f'Product name: {prod.name}')
-    print(f'Product identifier: {prod.identifier}')
-    print(f'Product price: {prod.price}')
-    print(f'Product weight: {prod.weight}')
-    print(f'Flammability: {prod.flammability}')
-
-    print(prod.stealability())
 
 class BoxingGlove(Product):
     '''create child class'''
-    def __init__(self, name, identifier, price, weight, flammability):
-        super().__init__(name, identifier, price, weight, flammability)
-    weight = 10
+    def __init__(self,
+                 name,
+                 identifier=random.randint(1000000, 9999999),
+                 price=10,
+                 weight=10,
+                 flammability=0.5) -> None:
+        super().__init__(name,
+                         identifier,
+                         price,
+                         weight,
+                         flammability)
 
     def explode(self):
         '''fun medthod docstring'''
@@ -58,10 +55,9 @@ class BoxingGlove(Product):
 
     def punch(self):
         '''func method docstring'''
-        weight = 10
-        if weight < 5:
-            return 'That tickles'
-        if weight >= 5 and weight > 15:
+        if self.weight < 5:
+            return 'That tickles.'
+        elif 5 <= self.weight < 15:
             return 'Hey that hurt!'
-        return 'OUCH!'
-        
+        else:
+            return 'OUCH!'
--- bloomdata/acme_report.py
+++ bloomdata/acme_report.py
@@ -1,38 +1,51 @@
+'''Imports'''
 import random
 from acme import Product
-'''imports'''
 
+# Random lists
 ADJECTIVES = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
 NOUNS = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
 
 
 def generate_products(num_products=30):
+    '''generate products with random attributes'''
     products = []
 
-    ran_fname = random.choice(ADJECTIVES)
-    ran_lname = random.choice(NOUNS)
-    ran_name = ran_fname + ' ' + ran_lname
-
-    ran_price = random.randint(5, 100)
-
-    ran_weight = random.randint(5, 100)
-
-    ran_flammability = random.uniform(0.0, 2.5)
-
     for _ in range(num_products):
-        products.append(Product(
-            ran_name, 0, ran_price, ran_weight, ran_flammability))
+        name = random.choice(ADJECTIVES) + ' ' + random.choice(NOUNS)
 
+        identifier = random.randint(1000000, 9999999)
+
+        price = random.randint(5, 100)
+
+        weight = random.randint(5, 100)
+
+        flammability = round(random.uniform(0.0, 2.5), 2)
+
+        products.append(Product(name, identifier, price,
+                                weight, flammability))
     return products
 
 
 def inventory_report(products):
-    unique_products = set(products)
-    return len(unique_products)
+    '''create an inventory report of products'''
+    names = []
+    total_price = 0
+    total_weight = 0
+    total_flammability = 0.0
 
+    for product in products:
+        names.append(product.name)
+        total_price += product.price
+        total_weight += product.weight
+        total_flammability += product.flammability
+
+    avg_price = total_price / len(products)
+    avg_weight = total_weight / len(products)
+    avg_flammability = total_flammability / len(products)
+    unique_names = set(names)
+    return (len(unique_names), avg_price, avg_weight, avg_flammability)
 
 
 if __name__ == '__main__':
- #  print(inventory_report(generate_products()))
- #  print(inventory_report(generate_products()))
-    print(generate_products(4))
+    print(inventory_report(generate_products(30)))
